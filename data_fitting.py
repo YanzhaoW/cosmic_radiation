45da14565331aca20b9cf96a05afce4c9aaee0f6
YanzhaoW <yannzhaow@gmail.com>
2023-06-29 15:07:51 +0200

    add confidence level

--- data_fitting.py
+++ data_fitting.py
@@ -1,40 +1,60 @@
+import numpy as np
 import pandas as pd
 from typing import Callable
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import odr
+from scipy import odr, stats
 
-class Fitter:
+class ODR_fitter:
     def __init__(self, dataframe : pd.DataFrame):
         self.dataframe = dataframe
+        self.dataframe.replace(0, np.nan, inplace = True)
 
-    def linear(self, fnt : Callable):
+    def fit(self, fnt : Callable, inits : list[float]):
+        self.fnt = fnt
         if self.dataframe.empty:
             raise ValueError("Cannot fit empty data!")
-        model = odr.Model(fnt)
+        model = odr.Model(self.fnt)
         data = odr.RealData(x = self.dataframe['x'], y = self.dataframe['y'], sx = self.dataframe['x_err'], sy = self.dataframe['y_err'])
-        ODR_reg = odr.ODR(data, model, beta0 = [1., 0.])
-        self.res = ODR_reg.run()
+        self.odr_reg = odr.ODR(data, model, beta0 = inits)
+        self.res = self.odr_reg.run()
+        res_var = self.odr_reg.output.__getattribute__('res_var')
+        self.p_value = 100*(1 - stats.chi2.cdf(res_var, df = 1))
 
-    def save_plot(self):
+    def save_plot(self, filename : str):
         sns.scatterplot(data = self.dataframe, x= 'x', y = 'y', label = "Data")
-        plt.errorbar(self.dataframe['x'], self.dataframe['y'], 
-                     xerr = self.dataframe['x_err'], yerr = self.dataframe['y_err'], 
+        dataframe = self.dataframe.replace(np.nan, 0)
+        plt.errorbar(dataframe['x'], dataframe['y'], 
+                     xerr = dataframe['x_err'], yerr = dataframe['y_err'], 
                      linestyle = 'None', capsize = 5.0)
-        slope = self.res.beta[0]
-        offset = self.res.beta[1]
-        fitted_fun = lambda x: slope*x + offset
-        x = self.dataframe['x'].iloc[[0, -1]]
+        fitted_fun = lambda x: self.fnt(self.res.beta, x)
+        x_range = dataframe['x'].iloc[[0, -1]].values
+        x = np.linspace(x_range[0], x_range[1], 1000)
         plt.plot(x, fitted_fun(x), color = 'r', label = "Regression line")
+        plt.plot([], [], linestyle = 'None'  ,label = "p-value: %2.2f%%" % self.p_value)
         plt.legend(loc = "upper left")
-        plt.savefig('fitting_plot.png', dpi = 200)
+        plt.savefig(filename, dpi = 200)
+        plt.close()
 
     def print(self):
+        print("===============fitting result:=================")
         self.res.pprint()
+        print("p-value: %.2f%%" % self.p_value)
+        print("===============================================")
 
 if __name__ == "__main__":
     dataframe = pd.read_csv('data.csv')
-    fitter = Fitter(dataframe)
-    fitter.linear(fnt = lambda p,x : p[0]* x + p[1])
-    fitter.print()
-    fitter.save_plot()
+    linear_fitter = ODR_fitter(dataframe)
+    linear_fitter.fit(fnt = lambda p,x : p[0]* x + p[1], inits = [1., 0.])
+    linear_fitter.print()
+    linear_fitter.save_plot('fitting_plot.png')
+
+    dataframe = pd.read_csv('trig_data.csv')
+    trig_fitter = ODR_fitter(dataframe)
+    trig_fitter.fit(fnt = lambda p,x : np.power( p[1]* (np.cos( p[0] * x) + 1), 2), inits = [1, 1])
+    trig_fitter.print()
+    trig_fitter.save_plot('trig_fitting.png')
+
+    trig_fitter.fit(fnt = lambda p,x : p[0]*x*x + p[1], inits = [-1, 1])
+    trig_fitter.print()
+    trig_fitter.save_plot('trig_fitting_quadratic.png')
--- data_generator.py
+++ data_generator.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
+import math
 from typing import Callable
 
 def Linear_fun(slope : float, bias: float) -> Callable:
     return lambda x: slope * x + bias
+
+def Trig_fun(power: float, k : float) -> Callable:
+    return lambda x : (math.cos(k * x) + 1) ** power
 
 class Random_Gen:
     def __init__(self, call_back : Callable):
@@ -40,21 +44,35 @@ class Random_Gen:
         errs = np.add(errs, self.yerror_min)
         return errs
 
-    def __call__(self, num : int, min: float, max: float):
-        x_err = self.__generate_xerr(num)
-        y_err = self.__generate_yerr(num)
-        x_mean = np.arange(start = min, stop = max, step = (max-min)/num, dtype = float)
-        y_mean = self.call_back(x_mean)
+    def __call__(self, size : int, x_min: float, x_max: float):
+        x_err = self.__generate_xerr(size)
+        y_err = self.__generate_yerr(size)
+        x_mean = np.linspace(start = x_min, stop = x_max, num = size, dtype = float)
+        y_mean = np.array(list(map(self.call_back, x_mean)))
+
+        if np.any(np.iscomplex(y_mean)):
+            print(y_mean)
+            raise ValueError("Complex error occured!")
+
         x = self.__generate_random(means = x_mean, errs = x_err)
         y = self.__generate_random(means = y_mean, errs = y_err)
         self.dataframe = pd.DataFrame({'x': x, 'x_err': x_err, 'y': y, 'y_err': y_err})
 
-    def SaveToCsv(self, filename : str = "data.csv"):
+    def SaveToCsv(self, filename : str):
         self.dataframe.to_csv(filename, index = False, float_format = '%.3f')
 
 if __name__ == "__main__":
     generator = Random_Gen(Linear_fun(2.3, 5.2))
     generator.set_xerror(min = 0.5, max = 1.2)
     generator.set_yerror(min = 4, max = 5)
-    generator(num = 20, min = 2, max = 23)
-    generator.SaveToCsv()
+    generator(size = 20, x_min = 2, x_max = 23)
+    generator.SaveToCsv("data.csv")
+
+    period = 3.2
+    power = 2
+    trig = Random_Gen(Trig_fun(power = power, k = 2 * math.pi / period))
+    trig.set_xerror(min = 0.005, max = 0.005)
+    trig.set_yerror(min = 0.1, max = 0.1)
+    trig(size = 20, x_min = - period / 2, x_max = period / 2)
+    trig.SaveToCsv("trig_data.csv")
+
